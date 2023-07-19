519ef744ec4ef3945f7c9d7d7db3150266655f07
Rickard Zachrisson <rizach8@gmail.com>
2023-07-19 22:05:31 +0200

    Compatibility: Able to disable breaking flint from TrueHardcore

--- Code/Mods/Compatilibity/TrueHardcore.cs
+++ Code/Mods/Compatilibity/TrueHardcore.cs
@@ -1,28 +1,31 @@
-﻿namespace Vheos.Mods.Outward;
+﻿using HardcoreRebalance;
+
+namespace Vheos.Mods.Outward;
 
 public class TrueHardcore : AMod
 {
     #region Constants
-    private static readonly int[] ITEMS =
-    {
-        ((int)HardcoreRebalance.HardcoreItems.ElattPiece)
-    };
+    public static readonly UID mortalityStatusUID = "4BN0PSfq2kaTEQTzvOy3Nw";
     #endregion
     #region Enums
     #endregion
 
     public static ModSetting<bool> _trueHardcoreEnabled;
-    public static readonly UID mortalityStatusUID = "4BN0PSfq2kaTEQTzvOy3Nw";
+    public static ModSetting<bool> _disableBreakFlint;
     protected override void Initialize()
     {
-        _trueHardcoreEnabled = CreateSetting(nameof(_trueHardcoreEnabled), false);
+        _trueHardcoreEnabled    = CreateSetting(nameof(_trueHardcoreEnabled), false);
+        _disableBreakFlint      = CreateSetting(nameof(_disableBreakFlint), false);
     }
     protected override void SetFormatting()
     {
         _trueHardcoreEnabled.Format("Enable TrueHardcore");
+        _disableBreakFlint.Format("Disable TrueHardcore flint break.");
+        _disableBreakFlint.Description = "This module already has support for Flint break with better control.";
     }
     protected override string Description
-    => "• Fixed: Manual Crafting";
+    => "• Fixed: Manual Crafting\n" +
+       "• Fixed: Disable flint breaking";
     protected override string SectionOverride
     => ModSections.Compatibility;
     protected internal override void LoadPreset(string presetName)
@@ -32,7 +35,13 @@ public class TrueHardcore : AMod
             case nameof(Preset.Vheos_CoopSurvival):
                 ForceApply();
                 _trueHardcoreEnabled.Value = false;
+                _disableBreakFlint.Value = false;
                 break;
         }
     }
+
+    // Hooks
+    [HarmonyPrefix, HarmonyPatch(typeof(DeathManager.Item_OnUse), nameof(DeathManager.Item_OnUse.Postfix))]
+    private static bool DeathManager_Item_OnUse_Pre()
+        => _trueHardcoreEnabled.Value && !_disableBreakFlint.Value;
 }
